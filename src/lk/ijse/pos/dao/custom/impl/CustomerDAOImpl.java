2862b4b4a98419a8ba07e89417c96cfcb3ee2f12
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-09-06 21:39:26 +0530

    Update CustomerDAOImpl.java

--- src/lk/ijse/pos/dao/custom/impl/CustomerDAOImpl.java
+++ src/lk/ijse/pos/dao/custom/impl/CustomerDAOImpl.java
@@ -4,27 +4,62 @@ import lk.ijse.pos.dao.custom.CustomerDAO;
 import lk.ijse.pos.entity.Customer;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class CustomerDAOImpl implements CustomerDAO {
     @Override
     public ArrayList<Customer> getAll(Connection connection) throws ClassNotFoundException, SQLException {
-        return null;
+        ArrayList<Customer> customers = new ArrayList<>();
+
+        PreparedStatement pstm = connection.prepareStatement("select * from customerinfo");
+        ResultSet resultSet = pstm.executeQuery();
+
+        while (resultSet.next()) {
+            customers.add(
+                    new Customer(
+                            resultSet.getString(1),
+                            resultSet.getString(2),
+                            resultSet.getString(3),
+                            resultSet.getString(4)
+
+                    )
+            );
+        }
+
+        return customers;
     }
 
     @Override
-    public boolean save(Customer data, Connection connection) throws ClassNotFoundException, SQLException {
-        return false;
+    public boolean save(Customer customer, Connection connection) throws ClassNotFoundException, SQLException {
+
+        PreparedStatement pstm = connection.prepareStatement("insert into customerinfo values(?,?,?,?)");
+        pstm.setObject(1, customer.getCusID());
+        pstm.setObject(2, customer.getName());
+        pstm.setObject(3, customer.getAddress());
+        pstm.setObject(4, customer.getContact());
+
+        return pstm.executeUpdate() > 0;
     }
 
     @Override
     public boolean delete(String id, Connection connection) throws ClassNotFoundException, SQLException {
-        return false;
+        PreparedStatement pstm = connection.prepareStatement("delete from customerinfo where cusID=?");
+        pstm.setObject(1, id);
+
+        return pstm.executeUpdate() > 0;
     }
 
     @Override
-    public boolean update(Customer data, Connection connection) throws ClassNotFoundException, SQLException {
-        return false;
+    public boolean update(Customer customer, Connection connection) throws ClassNotFoundException, SQLException {
+        PreparedStatement pstm = connection.prepareStatement("update customerinfo set name=?,address=?,contact=? where cusID=?");
+        pstm.setObject(4, customer.getCusID());
+        pstm.setObject(1, customer.getName());
+        pstm.setObject(2, customer.getAddress());
+        pstm.setObject(3, customer.getContact());
+
+        return pstm.executeUpdate() > 0;
     }
 }
