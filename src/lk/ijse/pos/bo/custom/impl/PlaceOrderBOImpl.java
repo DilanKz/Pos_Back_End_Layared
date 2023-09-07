b034b80b3c103a42ed0131c077f2a5ca79e46320
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-09-07 21:48:35 +0530

    Update PlaceOrderBOImpl.java

--- src/lk/ijse/pos/bo/custom/impl/PlaceOrderBOImpl.java
+++ src/lk/ijse/pos/bo/custom/impl/PlaceOrderBOImpl.java
@@ -9,11 +9,15 @@ import lk.ijse.pos.dao.custom.OrdersDAO;
 import lk.ijse.pos.dto.CustomerDTO;
 import lk.ijse.pos.dto.ItemDTO;
 import lk.ijse.pos.dto.OrderDTO;
+import lk.ijse.pos.dto.OrderDetailsDTO;
 import lk.ijse.pos.entity.Customer;
 import lk.ijse.pos.entity.Item;
+import lk.ijse.pos.entity.OrderDetails;
+import lk.ijse.pos.entity.Orders;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class PlaceOrderBOImpl implements PlaceOrderBO {
     CustomerDAO customerDAO = (CustomerDAO) DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.Customer);
@@ -35,6 +39,41 @@ public class PlaceOrderBOImpl implements PlaceOrderBO {
 
     @Override
     public boolean saveOrder(OrderDTO orderDTO, Connection connection) throws SQLException, ClassNotFoundException {
-        return false;
+        //getting all the necessary data
+        Orders orders = new Orders(orderDTO.getId(), orderDTO.getDate(), orderDTO.getTotal(), orderDTO.getCusID());
+
+        ArrayList<OrderDetails> orderDetails = new ArrayList<>();
+
+        for (OrderDetailsDTO dto : orderDTO.getList()) {
+            orderDetails.add(
+                    new OrderDetails(
+                            dto.getOrderID(),
+                            dto.getCode(),
+                            dto.getItemQty(),
+                            dto.getUnitPrice()
+                    )
+            );
+        }
+
+        //transaction
+        try {
+            connection.setAutoCommit(false);
+
+            //save order
+            if (ordersDAO.save(orders,connection)) {
+                //save details
+                if (orderDetailsDAO.saveDetails(orderDetails,connection)) {
+                    //update items
+                    if (itemDAO.updateAllItems(orderDetails,connection)) {
+                        connection.commit();
+                        return true;
+                    }
+                }
+            }
+            connection.rollback();
+            return false;
+        } finally {
+            connection.setAutoCommit(true);
+        }
     }
 }
