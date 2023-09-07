5d889b2635c1e7041644519f98d24950fa40ace6
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-09-07 17:42:16 +0530

    Create DAOFactory.java

---
+++ src/lk/ijse/pos/dao/DAOFactory.java
@@ -0,0 +1,41 @@
+package lk.ijse.pos.dao;
+
+import lk.ijse.pos.dao.custom.impl.CustomerDAOImpl;
+import lk.ijse.pos.dao.custom.impl.ItemDAOImpl;
+import lk.ijse.pos.dao.custom.impl.OrderDAOImpl;
+import lk.ijse.pos.dao.custom.impl.OrderDetailsDAOImpl;
+
+public class DAOFactory {
+    private static DAOFactory factory;
+
+    private DAOFactory(){
+
+    }
+    public static DAOFactory getInstance(){
+        if (factory==null){
+            factory=new DAOFactory();
+        }
+        return factory;
+    }
+
+
+    public enum DAOTypes{
+        Customer,Items,Orders,OrderDetails,Quarry
+    }
+    public SuperDAO getDAO(DAOTypes daoTypes) {
+        switch (daoTypes) {
+            case Customer:
+                return new CustomerDAOImpl();
+            case Items:
+                return new ItemDAOImpl();
+            case Orders:
+                return new OrderDAOImpl();
+            case OrderDetails:
+                return new OrderDetailsDAOImpl();
+            case Quarry:
+                //Quarry dao
+            default:
+                return null;
+        }
+    }
+}
