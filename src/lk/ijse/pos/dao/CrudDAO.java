07d0340439ecaecccca2b653888dc826bb7c366b
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-09-07 17:42:35 +0530

    Update CrudDAO.java

--- src/lk/ijse/pos/dao/CrudDAO.java
+++ src/lk/ijse/pos/dao/CrudDAO.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public interface CrudDAO <T>{
+public interface CrudDAO <T> extends SuperDAO{
     ArrayList<T> getAll(Connection connection) throws ClassNotFoundException , SQLException;
     boolean save(T data, Connection connection) throws ClassNotFoundException , SQLException;
     boolean delete(String id,Connection connection) throws ClassNotFoundException , SQLException;
